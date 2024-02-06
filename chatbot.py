651c297d07e8b17d0aa65e10182fae762184337b
Wai Hong Wong <waihongwong@Wais-MacBook-Air.local>
2024-02-06 11:20:47 +0800

    lab 4

--- chatbot.py
+++ chatbot.py
@@ -3,8 +3,20 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 import configparser
 import logging
 import redis
+from ChatGPT_HKBU import HKBU_ChatGPT
+import logging
 
 global redis1
+global chatgpt
+
+def equiped_chatgpt(update, context):
+    global chatgpt
+    reply_message = chatgpt.submit(update.message.text)
+    logging.info("Update: " + str(update))
+    logging.info("Context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+
 
 def main():
     # Load your token and create an Updater for your Bot
@@ -20,9 +32,15 @@ def main():
     # You can set this logging module, so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-    # Register a dispatcher to handle messages: here we register an echo dispatcher
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
+
+    # Register a dispatcher to handle message: here we register an echo dispatcher
+    # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    # dispatcher.add_handler(echo_handler)
+    # Dispatcher for chatgpt
+    global chatgpt
+    chatgpt = HKBU_ChatGPT(config)
+    chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
+    dispatcher.add_handler(chatgpt_handler)
 
     # On different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
